0fef08e4b26b77b33e7031d39cf6065d36943e4c
guseynov <guseynov.smm@gmail.com>
2023-03-13 13:10:57 +0500

    0. Что такое переменная

---
+++ src/main/java/ru/job4j/Variable.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+public class Variable {
+    public static void main(String[] args) {
+        int age = 18;
+        float length = 78.0332F;
+        boolean baby = true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Length: " + length);
+        System.out.println("Baby: " + baby);
+    }
+}
