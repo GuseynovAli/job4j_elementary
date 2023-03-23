691ba8ebc1a67bbe4c843d2f8ba78ef9922bd008
guseynov <guseynov.smm@gmail.com>
2023-03-23 13:10:12 +0500

    1.2. Входные данные в тесте

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -42,8 +42,8 @@ public class X2Test {
     public void whenA1B1C1X0Then2() {
         int a = 1;
         int b = 1;
-        int c = 0;
-        int x = 1;
+        int c = 1;
+        int x = 0;
         int expected = 2;
         int rsl = X2.calc(a, b, c, x);
         Assert.assertEquals(expected, rsl);
