973291658ca830fdcda5bc092c4a5a1515e9f748
guseynov <guseynov.smm@gmail.com>
2023-03-23 13:57:41 +0500

    4. Тесты для стороны прямоугольника.

--- src/main/java/ru/job4j/Variable.java
+++ src/main/java/ru/job4j/Variable.java
@@ -3,11 +3,12 @@ package ru.job4j;
 public class Variable {
     public static void main(String[] args) {
         int age = 18;
-        float length = 78.0332F;
-        boolean baby = true;
-
-        System.out.println("Age: " + age);
-        System.out.println("Length: " + length);
-        System.out.println("Baby: " + baby);
+        char sex = 'M';
+        float height = 1.82f;
+        short bricks = 16530;
+        double bacteria = 234234.234234;
+        double way = 23420834234234.9835763934;
+        boolean work = true;
+        long file = 34897539485234234L;
     }
 }
