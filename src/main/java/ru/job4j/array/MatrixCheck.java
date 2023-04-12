18d88a9e5d96e3dd3d0256d04f25f00bce6ee49a
guseynov <guseynov.smm@gmail.com>
2023-04-12 15:52:52 +0500

    6.7.4. Выигрышные комбинации в сокобан

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,15 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
+    public static boolean isWin(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X') {
+                return monoHorizontal(board, i) || monoVertical(board, i);
+            }
+        }
+        return false;
+    }
+
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
         for (int cell = row; cell < board.length; cell++) {
